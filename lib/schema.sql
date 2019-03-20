875476988d2aed1c17f5f0266046482c2d0ca418
Bryn Bennett <bryn.bennett@gmail.com>
2019-03-20 19:27:01 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -2,27 +2,47 @@ CREATE TABLE series (
   id INTEGER PRIMARY KEY,
   title TEXT,
   author_id INTEGER,
+<<<<<<< HEAD
   subgenre_id INTEGER
 );
 
 CREATE TABLE subgenres (
+=======
+  sub_genre_id INTEGER
+);
+
+CREATE TABLE sub_genre (
+>>>>>>> 2ad29c765113d7499638a0fc1fa34ff0073f9ae1
   id INTEGER PRIMARY KEY,
   name TEXT
 );
 
+<<<<<<< HEAD
 CREATE TABLE authors(
   id INTEGER PRIMARY KEY,
   name TEXT
 );
 
 CREATE TABLE books(
+=======
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name text
+);
+
+CREATE TABLE books (
+>>>>>>> 2ad29c765113d7499638a0fc1fa34ff0073f9ae1
   id INTEGER PRIMARY KEY,
   title TEXT,
   year INTEGER,
   series_id INTEGER
 );
 
+<<<<<<< HEAD
 CREATE TABLE characters(
+=======
+CREATE TABLE characters (
+>>>>>>> 2ad29c765113d7499638a0fc1fa34ff0073f9ae1
   id INTEGER PRIMARY KEY,
   name TEXT,
   motto TEXT,
@@ -31,8 +51,15 @@ CREATE TABLE characters(
   series_id INTEGER
 );
 
+<<<<<<< HEAD
 CREATE TABLE character_books(
   id INTEGER PRIMARY KEY,
   character_id INTEGER,
   book_id INTEGER
+=======
+CREATE TABLE character_books (
+  id INTEGER PRIMARY KEY,
+  character_id INTEGER,
+  book_id INTEGER,
+>>>>>>> 2ad29c765113d7499638a0fc1fa34ff0073f9ae1
 );
